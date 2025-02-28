31c591eaa0553bde7fd16a7fc0f7590671d7b6e0
Vu Binh <22520129@gm.uit.edu.vn>
2025-02-28 21:05:54 +0700

    Ex 0010: Complete

---
+++ BaiTap/src/Bai_010.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bai_010 {
+    public static void main(String[] args) {
+        float x1, x2, x3, y1, y2, y3;
+        System.out.print("Nhập x1: ");
+        x1 = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập y1: ");
+        y1 = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập x2: ");
+        x2 = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập y2: ");
+        y2 = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập x3: ");
+        x3 = new Scanner(System.in).nextFloat();
+        System.out.print("Nhập y3: ");
+        y3 = new Scanner(System.in).nextFloat();
+        double a = Math.sqrt((Math.pow(x3-x2,2)) + Math.pow(y3 - y2,2));
+        double b = Math.sqrt((Math.pow(x1-x2,2)) + Math.pow(y1 - y2,2));
+        double c = Math.sqrt((Math.pow(x3-x1,2)) + Math.pow(y3 - y1,2));
+        double cv = a + b + c;
+        System.out.println("Chu vi tam giác là: " + cv);
+    }
+}
