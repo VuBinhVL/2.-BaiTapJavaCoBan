2b552d6c959d8364c43577c241c8060384ae9094
Vu Binh <22520129@gm.uit.edu.vn>
2025-04-03 08:40:25 +0700

    Bào tập về lớp Thread: Hoàn thành bài 2

---
+++ BaiTap4/src/Bai_02.java
@@ -0,0 +1,26 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Bai_02 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            while (true)
+            {
+                LocalTime now = LocalTime.now();
+                System.out.println("Thời gian hiện tại: " + now.format(dateTimeFormatter));
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Lỗi khi lấy thời gian! " + e.getMessage());
+                }
+            }
+        }
+        );
+        thread.start();
+    }
+}
